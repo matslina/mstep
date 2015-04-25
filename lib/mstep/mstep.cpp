245a1cef318aff2a918f46ab4cd5a748eef8f06a
Mats Linander <matslina@gmail.com>
2015-04-25 14:43:22 -0400

    remove garbage

--- lib/mstep/mstep.cpp
+++ lib/mstep/mstep.cpp
@@ -1,32 +1,5 @@
 #include "mstep.hpp"
 
-#if 0
-class Grid {
- public:
-  virtual char *getEvents() = 0;
-  virtual void clearEvents() = 0;
-  virtual void draw(char *grid) = 0;
-  virtual char getWidth() = 0;
-  virtual char getHeight() = 0;
-};
-
-
-
-class Control {
- public:
-  virtual ~Control() {}
-  virtual bool getButtonEvents() = 0;
-  virtual void clearButtonEvents() = 0;
-  virtual bool shutdownRequested() = 0;
-};
-
-class Display {
- public:
-  virtual void write() = 0;
-};
-
-#endif
-
 MStep::MStep(Grid *grid, Control *control, Display *display,
 	     void (*sleep)(unsigned long)) {
   this->grid = grid;
