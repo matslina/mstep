2468c9c9241332a0fcff940ba829bd8099013b32
Mats Linander <matslina@gmail.com>
2015-08-02 17:12:47 -0400

    overhaul of adafruit implementation
    - I2C switching to control > 8 trellis grids
      - in our case 12 of them in a 16x12 pad configuration
      - two gpio pins control transistor based switcg
      - also tweaked the pad/led mapping macros a bit
    
    - Shift registers for the controls
      - 4021 shift register multiplexes 8 switches
      - only mapped these to play, note, tempo, up and down for now
      - custom shiftIn method coz arduino's builtin doesn't play nice w
        the 4021
    
    - LCD implementation for the display
      - for a 16x2 HD44780 LCD
    
    - Broke out initialization logic from setup() to methods

--- src/adafruit.ino
+++ src/adafruit.ino
@@ -1,3 +1,4 @@
+#include <LiquidCrystal.h>
 #include <Adafruit_Trellis.h>
 #include <mstep.hpp>
 
@@ -11,31 +12,89 @@ Adafruit_Trellis matrix4 = Adafruit_Trellis();
 Adafruit_Trellis matrix5 = Adafruit_Trellis();
 Adafruit_Trellis matrix6 = Adafruit_Trellis();
 Adafruit_Trellis matrix7 = Adafruit_Trellis();
-Adafruit_TrellisSet trellis =  Adafruit_TrellisSet(&matrix0, &matrix1,
-                                                   &matrix2, &matrix3,
-                                                   &matrix4, &matrix5,
-                                                   &matrix6, &matrix7);
+Adafruit_Trellis matrix8 = Adafruit_Trellis();
+Adafruit_Trellis matrix9 = Adafruit_Trellis();
+Adafruit_Trellis matrixA = Adafruit_Trellis();
+Adafruit_Trellis matrixB = Adafruit_Trellis();
+Adafruit_TrellisSet trellis0 = Adafruit_TrellisSet(&matrix0, &matrix1,
+						   &matrix2, &matrix3);
+Adafruit_TrellisSet trellis1 = Adafruit_TrellisSet(&matrix4, &matrix5,
+						   &matrix6, &matrix7,
+						   &matrix8, &matrix9,
+						   &matrixA, &matrixB);
 
-#define LEDAT(row, col) ((col / 4) * 16) + (col % 4) + (4 * row) + ((row / 4) * 48)
-#define LEDROW(i) ((i >> 6) << 2) | ((i & 0x0f) >> 2)
-#define LEDCOL(i) (((i & 0x3f) >> 4 ) << 2) | (i & 0x03)
+
+
+#define LEDAT(row, col) (((((col) >> 2) << 4) |			\
+			  ((col) & 3)) |			\
+			 ((row) << 2) +				\
+			 ((row) >> 2) * 48)
+#define LEDROW(i) (((i) >> 6) << 2) | (((i) & 0x0f) >> 2)
+#define LEDCOL(i) (((i) & 0x30) >> 2) | ((i) & 0x03)
 
 
 class AdaGrid : public Grid {
+ public:
+  bool lower;
+
+  void initialize() {
+    digitalWrite(5, HIGH);
+    digitalWrite(4, HIGH);
+    lower = true;
+    switchI2C(false);
+    trellis0.begin(0x70, 0x71, 0x72, 0x73);
+    switchI2C(true);
+    trellis1.begin(0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77);
+  }
+
+  void switchI2C(bool lower) {
+    if (lower == this->lower)
+      return;
+
+    if (lower) {
+      digitalWrite(4, HIGH);
+      digitalWrite(5, LOW);
+    } else {
+      digitalWrite(4, LOW);
+      digitalWrite(5, HIGH);
+    }
+
+    this->lower = lower;
+  }
+
   bool getPress(char *row, char *column) {
     static bool needRead = true;
     static int i;
+    bool changed;
 
     if (needRead) {
-      trellis.readSwitches();
+      switchI2C(true);
+      changed = trellis1.readSwitches();
+      switchI2C(false);
+      changed = changed || trellis0.readSwitches();
       needRead = false;
       i = 0;
     }
 
-    while (i < 16 * 8) {
-      if (trellis.justPressed(i)) {
+    if (!changed)
+      return false;
+
+    switchI2C(false);
+    while (i < 64) {
+      if (trellis0.justPressed(i)) {
 	*row = LEDROW(i);
 	*column = LEDCOL(i);
+	i++;
+	return true;
+      }
+      i++;
+    }
+
+    switchI2C(true);
+    while (i < 16 * 12) {
+      if (trellis1.justPressed(i - 64)) {
+	*row = 4 + LEDROW(i - 64);
+	*column = LEDCOL(i - 64);
 	i++;
 	return true;
       }
@@ -47,13 +106,24 @@ class AdaGrid : public Grid {
   }
 
   void draw(char *state) {
-    for (int i = 0; i < 16 * 8; i++) {
-      if (state[i / 8] & (1 << (i % 8)))
-	trellis.setLED(LEDAT((i / 16), (i % 16)));
+    switchI2C(false);
+    for (int i=0; i < 64; i++) {
+      if (state[i >> 3] & (1 << (i & 0x7)))
+	trellis0.setLED(LEDAT(i >> 4, i & 0xf));
       else
-	trellis.clrLED(LEDAT((i / 16), (i % 16)));
+	trellis0.clrLED(LEDAT(i >> 4, i & 0xf));
     }
-    trellis.writeDisplay();
+    trellis0.writeDisplay();
+
+    switchI2C(true);
+    for (int i = 16 * 4; i < 16 * 12; i++) {
+      int pad = i - 64;
+      if (state[i / 8] & (1 << (i % 8)))
+	trellis1.setLED(LEDAT(pad >> 4, pad & 0xf));
+      else
+	trellis1.clrLED(LEDAT(pad >> 4, pad & 0xf));
+    }
+    trellis1.writeDisplay();
   }
 
   char getWidth() {
@@ -61,11 +131,15 @@ class AdaGrid : public Grid {
   }
 
   char getHeight() {
-    return 8;
+    return 12;
   }
 };
 
 class AdaMIDI : public MIDI {
+ public:
+  void initialize() {
+    Serial1.begin(31250);
+  }
 
   void noteOn(int channel, int note, int velocity) {
     Serial1.write((unsigned char)(0x90 | channel));
@@ -76,27 +150,80 @@ class AdaMIDI : public MIDI {
 };
 
 class AdaControl : public Control {
+ public:
+  int up = 0;
+  int down = 0;
+
+  void initialize() {
+      pinMode(12, INPUT);
+      pinMode(11, OUTPUT);
+      pinMode(10, OUTPUT);
+      pinMode(9, OUTPUT);
+      pinMode(8, OUTPUT);
+      pinMode(7, OUTPUT);
+  }
+
+  // shifts in from 4021 w (clock, latch, data) = (10, 11, 12)
+  byte shiftIn() {
+    int i;
+    int temp = 0;
+    byte myDataIn = 0;
+
+    digitalWrite(11, HIGH);
+    delayMicroseconds(20);
+    digitalWrite(11, LOW);
+
+    for (i=7; i>=0; i--)
+      {
+	digitalWrite(10, 0);
+	delayMicroseconds(0.2);
+	temp = digitalRead(12);
+	if (temp)
+	  myDataIn = myDataIn | (1 << i);
+	digitalWrite(10, 1);
+      }
+    return myDataIn;
+  }
 
   void indicate(int event) {
+    // clock, latch, data = 7, 8, 9
+    digitalWrite(8, LOW);
+    shiftOut(9, 7, MSBFIRST, 0xf0);
+    digitalWrite(8, HIGH);
   }
 
   int getEvent() {
-    return 0;
+    byte b = this->shiftIn();
+    byte ret = 0;
+
+    if (b & 1)
+      ret |= Control::PLAY;
+    if (b & 2)
+      ret |= Control::NOTE;
+    if (b & 4)
+      ret |= Control::TEMPO;
+    if (b & 64)
+      up++;
+    if (b & 32)
+      down++;
+
+    return ret;
   }
 
   int getUp() {
-    return 0;
+    int up = this->up;
+    this->up = 0;
+    return up;
   }
 
   int getDown() {
-    return 0;
+    int down = this->down;
+    this->down = 0;
+    return down;
   }
 
-  bool eventPlayPause() {
-    static unsigned long lastLow = 0;
-    return click(PIN_PLAYPAUSE, &lastLow);
-  }
 
+  // keeping this old debouncing logic around for now
   bool click(int pin, unsigned long *lastLow) {
     bool low = digitalRead(pin) == LOW;
     unsigned long now;
@@ -122,11 +249,21 @@ class AdaControl : public Control {
 };
 
 class AdaDisplay : public Display {
+ public:
+  LiquidCrystal *lcd;
+
+  void initialize() {
+    lcd = new LiquidCrystal(A0, A1, A2, A3, A4, A5);
+    lcd->begin(16, 2);
+  }
 
   void write(int row, char *msg) {
+    lcd->setCursor(0, row);
+    lcd->print(msg);
   }
 
   void clear() {
+    lcd->clear();
   }
 };
 
@@ -138,12 +275,12 @@ AdaMIDI midi = AdaMIDI();
 MStep mstep = MStep(&grid, &control, &display, &midi, &delay, &millis);
 
 void setup() {
-  Serial1.begin(31250);
-  pinMode(PIN_PLAYPAUSE, INPUT);
-  trellis.begin(0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77);
+  grid.initialize();
+  display.initialize();
+  control.initialize();
+  midi.initialize();
 }
 
 void loop() {
   mstep.run();
-  delay(1000);
 }
