a5e6bda6d12b81836ad9ed42957a8bf3b8574a2f
Mats Linander <matslina@gmail.com>
2016-05-09 21:02:01 -0400

    break out player implementation to .cpp file

---
+++ lib/mstep/player.cpp
@@ -0,0 +1,103 @@
+#include "player.hpp"
+
+void Player::noteOff(int patternIndex) {
+  struct pattern_state *state = &allState[patternIndex];
+  pattern_t *pattern = &pc->program.pattern[patternIndex];
+
+  for (int i = 0; i < GRID_H; i ++) {
+    if (state->activeNote[i] >= 0) {
+      midi->noteOn(state->activeChannel, state->activeNote[i], 0);
+      state->activeNote[i] = -1;
+    }
+  }
+}
+
+void Player::noteOn(int patternIndex) {
+  pattern_t *pattern = &pc->program.pattern[patternIndex];
+  struct pattern_state *state = &allState[patternIndex];
+
+  for (int i = 0; i < GRID_H; i ++) {
+    int pad = i * GRID_W + state->column;
+    if (pattern->grid[pad / 8] & (1 << (pad % 8))) {
+      midi->noteOn(pattern->channel, pattern->note[i], pattern->velocity[i]);
+      state->activeNote[i] = pattern->note[i];
+    }
+  }
+  state->activeChannel = pattern->channel;
+}
+
+Player::Player(MIDI *midi,
+	       void (*sleep)(unsigned long),
+	       unsigned long (*time)(void),
+	       ProgramController *pc) {
+  this->midi = midi;
+  this->pc = pc;
+  this->sleep = sleep;
+  this->time = time;
+
+  for (int i = 0; i < GRID_H; i++)
+    for (int j = 0; j < GRID_H; j++)
+      allState[i].activeNote[j] = -1;
+}
+
+void Player::start() {
+  playIndex = pc->currentPattern;
+  allState[playIndex].swingDelay = 0;
+  allState[playIndex].column = 0;
+  nextEventTime = time();
+}
+
+void Player::stop() {
+  noteOff(playIndex);
+  pc->highlightColumn = -1;
+  pc->draw();
+}
+
+unsigned int Player::tick() {
+  int pad;
+  unsigned long int now;
+  unsigned long int when;
+  pattern_t *pattern = &pc->program.pattern[playIndex];
+  struct pattern_state *state = &allState[playIndex];
+
+  now = time();
+  when = nextEventTime + state->swingDelay;
+  if (when > now)
+    return when - now;
+
+  // send MIDI messages before doing anything else.
+  noteOff(playIndex);
+  noteOn(playIndex);
+
+  // if playing pattern is currently displayed: update the
+  // highlighted column.
+  pc->highlightColumn = -1;
+  if (playIndex == pc->currentPattern)
+    pc->highlightColumn = state->column;
+  pc->draw();
+
+  // step one column forward. if wrapping around, start playing the
+  // currently displayed pattern and bring along data so currently
+  // playing notes are turned off properly.
+  if (++state->column == GRID_W) {
+    if (playIndex != pc->currentPattern) {
+      playIndex = pc->currentPattern;
+      pattern = &pc->program.pattern[playIndex];
+      for (int i = 0; i < GRID_H; i++)
+	allState[playIndex].activeNote[i] = state->activeNote[i];
+      allState[playIndex].activeChannel = state->activeChannel;
+      state = &allState[playIndex];
+    }
+    state->column = 0;
+  }
+
+  // schedule next step
+  nextEventTime += (240000 / pc->program.tempo) / GRID_W;
+  state->swingDelay = 0;
+  if (!(state->column & 1))
+    state->swingDelay = (((float)(pattern->swing - 50) / 50) *
+			 (240000 / pc->program.tempo) / GRID_W);
+
+  return MAX(0, nextEventTime + state->swingDelay - time());
+}
+
